fc6a5d6d766df6a2655bcda7ee4e18b91ac96ddd
abdullah-sami <abdullahsami4103@gmail.com>
2026-04-21 11:35:30 +0600

    .

--- twimbol_app/twimbol/babel.config.js
+++ twimbol_app/twimbol/babel.config.js
@@ -6,6 +6,7 @@ module.exports = function (api) {
       "nativewind/babel",
     ],
     plugins: [
+      "react-native-worklets-core/plugin", // Add this line
       "react-native-reanimated/plugin",
     ],
   };
